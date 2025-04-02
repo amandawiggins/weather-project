421bdb8704e12aa28b20eb9e6255b8b1a23f16e9
Amanda Wiggins <wiggins.amanda@gmail.com>
2025-04-02 11:17:37 -0700

    add api key and url

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = f8bb49552a205a50dct88eo7f9d4363c;
+  let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
