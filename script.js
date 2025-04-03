297c8a895eaf3955c030c53344bde3d52ec2f38c
Amanda Wiggins <wiggins.amanda@gmail.com>
2025-04-03 10:36:25 -0700

    add icon api url

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  icon.innerHTML = ` <img src=${response.data.condition.icon_url}"class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
@@ -29,6 +31,9 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return `${day} ${hours} ${minutes}`;
 }
 
